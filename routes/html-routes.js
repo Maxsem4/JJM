ebd2f2bd5a7508f51a36c48919313f227a761add
John Blume <jlblumework@gmail.com>
2020-05-21 18:51:22 -0400

    Added most of the starter code excluding public.

---
+++ routes/html-routes.js
@@ -0,0 +1,15 @@
+// *********************************************************************************
+// html-routes.js
+// This module provides a set of routes for sending users the various
+// HTML pages for the client frontend.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+const path = require('path')
+const router = require('express').Router()
+
+// Routes
+// =============================================================
+
+module.exports = router
